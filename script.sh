df4bc6ad288f26fb1e50329e3a3ffd2fc8647648
Jakub Wilk <jakub_wilk@outlook.com>
2025-02-18 10:29:23 +0000

    Implemented info Output

--- script.sh
+++ script.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-
 metadataURL="http://169.254.169.254/latest/meta-data/"
 privIPv4Suffix="/local-ipv4"
 pubIPv4Suffix="/public-ipv4"
 instanceIDSuffix="/instance-id"
 secGroupsSuffix="/security-groups"
 
+function call_metadataAPI() {
+	curl -s "$metadataURL""$1"
+}
 
+privIPV4=$(curl -s "$metadataURL""$privIPv4Suffix")
+echo "Instance ID: "$(call_metadataAPI "$instanceIDSuffix")
+echo "Public IP: "$(call_metadataAPI "$pubIPv4Suffix")
+echo "Private IP: "$(call_metadataAPI "$privIPv4Suffix")
+echo "Security Groups: "$(call_metadataAPI "$secGroupsSuffix")
+echo "OS: "$(awk -F= '$1== "NAME" || $1== "VERSION" {gsub(/"/, "", $2); printf "%s ", $2}' /etc/os-release)
+echo "Users(with shell permissions): "$(awk -F: '$7 ~ /\/bin\/.*sh$/ {print $1}' /etc/passwd)
