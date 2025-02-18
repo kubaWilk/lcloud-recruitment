1df353a591a5105b4dcf7cdf278291a06abbd377
Jakub Wilk <jakub_wilk@outlook.com>
2025-02-18 10:35:24 +0000

    Info is now being saved to a file. Additionaly, deleted line with unused variable

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+fileName="output.txt"
 metadataURL="http://169.254.169.254/latest/meta-data/"
 privIPv4Suffix="/local-ipv4"
 pubIPv4Suffix="/public-ipv4"
@@ -9,10 +10,13 @@ function call_metadataAPI() {
 	curl -s "$metadataURL""$1"
 }
 
-privIPV4=$(curl -s "$metadataURL""$privIPv4Suffix")
-echo "Instance ID: "$(call_metadataAPI "$instanceIDSuffix")
-echo "Public IP: "$(call_metadataAPI "$pubIPv4Suffix")
-echo "Private IP: "$(call_metadataAPI "$privIPv4Suffix")
-echo "Security Groups: "$(call_metadataAPI "$secGroupsSuffix")
-echo "OS: "$(awk -F= '$1== "NAME" || $1== "VERSION" {gsub(/"/, "", $2); printf "%s ", $2}' /etc/os-release)
-echo "Users(with shell permissions): "$(awk -F: '$7 ~ /\/bin\/.*sh$/ {print $1}' /etc/passwd)
+function getInstanceInfo() {
+	echo "Instance ID: "$(call_metadataAPI "$instanceIDSuffix")
+	echo "Public IP: "$(call_metadataAPI "$pubIPv4Suffix")
+	echo "Private IP: "$(call_metadataAPI "$privIPv4Suffix")
+	echo "Security Groups: "$(call_metadataAPI "$secGroupsSuffix")
+	echo "OS: "$(awk -F= '$1== "NAME" || $1== "VERSION" {gsub(/"/, "", $2); printf "%s ", $2}' /etc/os-release)
+	echo "Users(with shell permissions): "$(awk -F: '$7 ~ /\/bin\/.*sh$/ {print $1}' /etc/passwd)
+}
+
+getInstanceInfo > "./$fileName"
