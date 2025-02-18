0e44dc440931414ee66f208421b791b590e1152e
Jakub Wilk <jakub_wilk@outlook.com>
2025-02-18 10:04:52 +0000

    Created vars for calling AWS API

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+metadataURL="http://169.254.169.254/latest/meta-data/"
+privIPv4Suffix="/local-ipv4"
+pubIPv4Suffix="/public-ipv4"
+instanceIDSuffix="/instance-id"
+secGroupsSuffix="/security-groups"
+
+
